42df76ad969be0f3867fba7e78d5b9e2cf8fe4ba
siamkhan-22 <siam0792khan@gmail.com>
2026-04-07 20:40:46 +0600

    Refactor validation logic and update event listeners

--- Lab Performance_MID/js/Validation.js
+++ Lab Performance_MID/js/Validation.js
@@ -1,30 +1,37 @@
-const unitPrice = 1000;
-const days = 30;
 
-let quantityInput = document.getElementById("quantity");
-let totalField = document.getElementById("total");
+const price = document.getElementById('uprice');
+const quantity = document.getElementById("qperday");
+const total = document.getElementById("tprice");
+const unit = 1000;
+const days = 30; 
 
-quantityInput.addEventListener("input", function(){
 
-    let quantity = parseInt(quantityInput.value);
+function calculator(){
+    let q = parseInt(quantity.value);
+    
 
-    if(quantity < 0){
-        alert("Quantity cannot be negative");
-        quantityInput.value = 0;
-        quantity = 0;
+    if (q < 0) {
+        alert("Quantity cannot be negative!");
+        q= 0;
+        quantity.value = 0;
     }
 
-    if(isNaN(quantity)){
-        totalField.value = 0;
+    else if(isNaN(q)) {
+        total.value = "";
         return;
     }
-
-    let totalPrice = unitPrice * quantity * days;
-
-    totalField.value = totalPrice;
-
-    if(totalPrice > 1000){
-        alert("Eligible for a Gift Coupon!");
+        let ctotal = unit * q * days;
+        total.value = ctotal;
+     if (ctotal > 1000) {
+        alert("Congratulations! You are eligible for a gift coupon");
     }
 
-});
+
+
+
+    
+}
+
+
+
+quantity.addEventListener('input',calculator );
