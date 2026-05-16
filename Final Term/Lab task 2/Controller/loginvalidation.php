71a8ee62afba091edd20597dee65feb5449dfaf6
siamkhan-22 <siam0792khan@gmail.com>
2026-05-17 04:11:17 +0600

    lab task 2

--- Final Term/Lab task 2/Controller/loginvalidation.php
+++ Final Term/Lab task 2/Controller/loginvalidation.php
@@ -1,1 +1,33 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    
+    <?php if(isset($_SESSION['success'])): ?>
+        <p style="color:green;"><?php echo $_SESSION['success']; unset($_SESSION['success']); ?></p>
+    <?php endif; ?>
+
+    <?php if(isset($_SESSION['error'])): ?>
+        <p style="color:red;"><?php echo $_SESSION['error']; unset($_SESSION['error']); ?></p>
+    <?php endif; ?>
+
+    <form action="../controllers/AuthController.php" method="POST">
+        <input type="hidden" name="action" value="login">
+        
+        <label>Email:</label><br>
+        <input type="email" name="email" required><br><br>
+        
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br><br>
+        
+        <button type="submit">Login</button>
+    </form>
+    <br>
+    <a href="register.php">Don't have an account? Register here.</a>
+</body>
+</html>
 
