c0f5443cd5550a17fb17824b5d054c3cbf37c6e2
siamkhan-22 <siam0792khan@gmail.com>
2026-05-17 04:33:57 +0600

    AJAX
    Added methods for user signup, signin, and username check with SQL injection protection.

--- Final Term/AJAX/model/db.php
+++ Final Term/AJAX/model/db.php
@@ -1,1 +1,51 @@
+<?php
+class db{
+function connection()
+{
+$db_host = "localhost";
+$db_user= "root";
+$db_password="";
+$db_name="section_f";
 
+$connection=  new mysqli($db_host, $db_user,$db_password,$db_name);
+if($connection->connect_error)
+    {
+        die ("Could not Connect Database".$connection->connect_error);
+    }
+return $connection;
+}
+
+function signup($connection, $tablename, $username, $password, $filepath)
+{
+    $sql= "INSERT INTO " .$tablename. "(username, password, filepath) VALUES ('".$username."', '".$password."','".$filepath."')";
+    $result = $connection->query($sql);
+    return $result;
+}
+function signin($connection, $tablename, $username, $password)
+{
+    $sql = "SELECT * FROM ".$tablename." WHERE username='".$username."' AND password='".$password."'";
+    $result = $connection->query($sql);
+    return $result;
+}
+
+function CheckUsername($connection, $tablename, $username)
+{
+    $sql = "SELECT * FROM ".$tablename." WHERE username='".$username."'";
+    $result = $connection->query($sql);
+    return $result;
+}	
+function WithSQLInjection($connection, $tablename, $username, $password, $filepath)
+{
+    $sql= "INSERT INTO " .$tablename. "(username, password, filepath) VALUES (?,?,?)";
+    $statement=$connection->prepare($sql);
+    $statement->bind_param("sss",$username, $password,$filepath);
+    $result = $statement->execute();
+    return $result;
+}
+
+
+
+}
+
+
+?>
